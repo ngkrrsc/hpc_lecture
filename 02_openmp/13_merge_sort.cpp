152e26bee3c769ee02b9d0bbf6dc0e7ab5498ec2
ngkrrsc <82810878+ngkrrsc@users.noreply.github.com>
2021-04-22 02:15:12 +0900

    Update 13_merge_sort.cpp

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -36,12 +36,18 @@ void merge_sort(std::vector<T>& vec, int begin, int end) {
 int main() {
   int n = 20;
   std::vector<int> vec(n);
+
   for (int i=0; i<n; i++) {
     vec[i] = rand() % (10 * n);
     printf("%d ",vec[i]);
   }
   printf("\n");
+#pragma omp parallel
+  {
+#pragma omp serial
   merge_sort(vec, 0, n-1);
+  }
+ 
   for (int i=0; i<n; i++) {
     printf("%d ",vec[i]);
   }
